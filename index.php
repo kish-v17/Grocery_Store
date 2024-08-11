1c8145f4646ccd699f4afe7971556e5ff255fcba
Kish-v17 <kish.v07@gmail.com>
2024-08-11 11:48:04 +0530

    color-updated

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php include('header.php'); ?>
 
-    <div class="hero">
-        <!-- <img src="img\items\banners\hero-bg.png" class="banner-bg" alt=""> -->
-        <img src="img\items\banners\Hero-img.png" alt="" class="hero-img">
-        
-    </div>
-
-
-
 <?php include('footer.php'); ?>
