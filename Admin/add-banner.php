2c69debcd5dd354a6a15a02b9ea98d75c6a54b90
harshilhirani0007 <harshilhiranipatel007@gmail.com>
2024-08-18 16:10:33 +0530

    error changes

--- Admin/add-banner.php
+++ Admin/add-banner.php
@@ -9,18 +9,20 @@
 
         <div class="card mb-4">
             <div class="card-body">
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="" method="POST" enctype="multipart/form-data" onsubmit="return validateAddBannerForm()"> 
                     <div class="row">
                         <div class="col-md-6">
                             <div class="mb-3">
                                 <label for="bannerImage" class="form-label">Banner Image</label>
                                 <input type="file" class="form-control" id="bannerImage" name="banner_image" required>
+                                <div id="bannerImageError" class="error-message"></div> <!-- Error message div -->
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="mb-3">
                                 <label for="bannerURL" class="form-label">Banner URL</label>
                                 <input type="url" class="form-control" id="bannerURL" name="banner_url" placeholder="https://example.com" required>
+                                <div id="bannerImageError" class="error-message"></div> <!-- Error message div -->
                             </div>
                         </div>
                     </div>
@@ -28,6 +30,7 @@
                     <div class="mb-3">
                         <label for="bannerOrder" class="form-label">View Order</label>
                         <input type="number" class="form-control" id="bannerOrder" name="banner_order" required>
+                        <div id="bannerImageError" class="error-message"></div> <!-- Error message div -->
                     </div>
 
                     <div class="mb-3">
