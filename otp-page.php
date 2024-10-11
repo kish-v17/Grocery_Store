432025bf42ec9c38242900d0f9de1a70d8c4f737
Kish-v17 <kish.v07@gmail.com>
2024-10-11 10:20:04 +0530

    mail

--- otp-page.php
+++ otp-page.php
@@ -1,4 +1,11 @@
-<?php include('header.php'); ?>
+<?php include('header.php');
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+?>
 
 
 <div class="container">
@@ -12,8 +19,10 @@
                         <input type="text" id="otp" name="otp" class="w-100" placeholder="OTP">
                         <p id="otpError" class="error  mb-4"></p>
                         <input type="submit" value="Verify" name="verify" class="btn-msg w-100">
+                    </form>
+                    <form id="resend" method="post">
                         <div class="mt-4 text-center">
-                            <a href="login.php" class="dim link ms-2">Back to log in</a>
+                            <button type="submit" name="resend_otp" class="otp ms-2">Resend OTP</button>
                         </div>
                     </form>
                 </div>
@@ -59,4 +68,45 @@ if (isset($_POST['verify'])) {
     }
 }
 
+if (isset($_POST['resend_otp'])) {
+    $mail = new PHPMailer(true);
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'purebitegroceryshop@gmail.com';
+        $mail->Password = 'lkge rlbk vtgd uaih';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = '587';
+
+        $mail->setFrom('purebitegroceryshop@gmail.com');
+        $mail->addAddress($regemail, $regfname);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Email Verification';
+        $otp = rand(100000, 999999);
+        $_SESSION['otp'] = $otp;
+        $_SESSION['otp_expiration'] = time() + 300;
+
+        $body = "<html>
+                    <body>
+                        <h2>Resend OTP for Email Verification</h2>
+                        <p>Dear {$_SESSION['user_data']['fname']},</p>
+                        <p>Your new OTP for email verification is: <strong>$otp</strong></p>
+                        <p>This OTP will expire in 5 minutes.</p>
+                        <p>If you didn't request this, please ignore this email.</p>
+                    </body>
+                </html>";
+
+        $mail->Body = $body;
+
+        if ($mail->send()) {
+            setcookie('success', 'New OTP has been sent to your email.', time() + 5, "/");
+        } else {
+            setcookie('error', "Failed to resend OTP: " . $mail->ErrorInfo, time() + 5, "/");
+        }
+    } catch (Exception $e) {
+        setcookie('error', "Error in sending email: " . $e->getMessage(), time() + 5, "/");
+    }
+}
 ?>
